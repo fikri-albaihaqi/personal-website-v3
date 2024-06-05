f7bebe083ebd1da67e0c6442167e9fac1a6bafa3
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-06-05 14:11:00 +0700

    feat: add necessary fonts

---
+++ app/assets/fonts.ts
@@ -0,0 +1,11 @@
+import { Cormorant_Garamond, Kumbh_Sans } from "next/font/google"
+
+export const cormorantGaramond = Cormorant_Garamond({
+  subsets: ['latin'],
+  weight: ['400', '600']
+})
+
+export const kumbhSans = Kumbh_Sans({
+  subsets: ['latin'],
+  weight: ['400']
+})
