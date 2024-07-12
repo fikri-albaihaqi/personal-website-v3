08412f9f9890cd09ecf790aa8e58204c190160b0
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-07-12 15:28:14 +0700

    feat: add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import Image from "next/image";
 import { cormorantGaramond } from "./assets/fonts";
 import Skills from "./components/skills";
 import Projects from "./components/projects";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [image, setImage] = useState<string>("")
+
+  const handleMouseEnter = (imageSource: string) => {
+    return () => {
+      setImage(imageSource)
+    }
+  }
+
   return (
     <>
       <header className={`h-screen flex justify-center ${cormorantGaramond.className}`}>
@@ -23,7 +35,7 @@ export default function Home() {
       </header>
 
       <main className="">
-        <section className={`h-screen flex justify-center items-center text-2xl text-center`}>
+        <section className="h-screen flex justify-center items-center text-2xl text-center">
           Hello! I’m Fikri, a 22 years old front end web developer <br />
           located in Bandung, Indonesia. I like to craft solid and <br />
           beautiful website with great user experience.
@@ -37,6 +49,41 @@ export default function Home() {
           </h1>
 
           <Projects />
+        </section>
+
+        <section className="h-screen flex justify-center items-center text-2xl text-center">
+          I&apos;m currently available <br />
+          and looking for a full-time remote position. <br />
+          If you’d like me to join your team, please reach me out!
+        </section>
+
+        <div className="fixed bottom-0 w-screen h-screen">
+          <Image
+            src={image}
+            alt="Social Background Image"
+            fill={true}
+            className={`overflow-hidden object-cover transition-all duration-500 ${image === "/dummy.png" ? "opacity-0" : "opacity-15"}`}
+          />
+        </div>
+
+        <section className="relative h-screen flex flex-col justify-center items-center text-center">
+
+          <h1 className="mb-24 text-3xl">Social</h1>
+
+          <ul className={`${cormorantGaramond.className} text-9xl font-semibold`}>
+            <Link href="mailto:fikrialbaihaqi@gmail.com" target='_blank' onMouseEnter={handleMouseEnter("/email.jpg")} onMouseLeave={handleMouseEnter("/dummy.png")} >
+              <li className="my-8">Email</li>
+            </Link>
+            <Link href="https://www.linkedin.com/in/fikri-albaihaqi/" target='_blank' onMouseEnter={handleMouseEnter("/linkedin.jpg")} onMouseLeave={handleMouseEnter("/dummy.png")}>
+              <li className="my-8">LinkedIn</li>
+            </Link>
+            <Link href="https://github.com/fikri-albaihaqi" target='_blank' onMouseEnter={handleMouseEnter("/github.jpg")} onMouseLeave={handleMouseEnter("/dummy.png")}>
+              <li className="my-8">Github</li>
+            </Link>
+            <Link href="https://codepen.io/Fikri-Code" target='_blank' onMouseEnter={handleMouseEnter("/codepen.jpg")} onMouseLeave={handleMouseEnter("/dummy.png")}>
+              <li className="my-8">Codepen</li>
+            </Link>
+          </ul>
         </section>
       </main>
     </>
