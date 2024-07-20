14c23cdcd3b7ed3c87eec542a6a9988ab32b222b
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2024-07-20 21:37:35 +0700

    feat: add aos animation on all projects page

--- app/allprojects/page.tsx
+++ app/allprojects/page.tsx
@@ -1,16 +1,25 @@
+'use client'
+
 import { cormorantGaramond } from "../assets/fonts"
 import projects from "../assets/projectsData"
 import Project from "../components/project"
+import AOS from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from "react";
 
 const AllProjects = () => {
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
   return (
     <main>
-      <h1 className={`${cormorantGaramond.className} text-8xl font-semibold ml-8 lg:ml-16 pt-32`}>
+      <h1 data-aos="fade-up" data-aos-duration="1000" className={`${cormorantGaramond.className} text-8xl font-semibold ml-8 lg:ml-16 pt-32`}>
         All <br /> 
         Works
       </h1>
 
-      <section className="relative md:grid grid-cols-2 gap-y-16 mx-8 lg:mx-16 my-32">
+      <section data-aos="fade-up" data-aos-duration="1000" data-aos-delay="350" className="relative md:grid grid-cols-2 gap-y-16 mx-8 lg:mx-16 my-32">
         {projects.map((project: any) => (
           <Project project={project} key={project.id} classes="lg:mx-4 odd:place-self-start even:place-self-end" />
         ))}
