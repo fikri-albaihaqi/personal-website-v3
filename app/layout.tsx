30b073b28cf2d50ce311d8bd626ebb5fe1338399
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2025-05-19 16:08:20 +0700

    feat: add lenis smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,34 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { kumbhSans } from "./assets/fonts";
-import Menu from "./components/menu";
-import Contact from "./components/contact";
+import type { Metadata } from 'next'
+import './globals.css'
+import { kumbhSans } from './assets/fonts'
+import Menu from './components/menu'
+import Contact from './components/contact'
+import { ReactLenis } from './utils/lenis'
 
 export const metadata: Metadata = {
-  title: "Fikri Albaihaqi",
-  description: "A 22 years old front end web developer located in Bandung, Indonesia.I like to craft solid and beautiful website with great user experience.",
+  title: 'Fikri Albaihaqi',
+  description:
+    'A 22 years old front end web developer located in Bandung, Indonesia.I like to craft solid and beautiful website with great user experience.',
   icons: {
-    icon: "/favicon.png",
-  }
-};
+    icon: '/favicon.png',
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${kumbhSans.className} text-ivory`}>
-        <div className="w-screen h-screen bg-[url('/bg-noise.gif')] opacity-10 fixed -z-50">
-        </div>
-        <Menu />
-        {children}
-        <Contact />
-      </body>
+      <ReactLenis root>
+        <body className={`${kumbhSans.className} text-ivory`}>
+          <div className="w-screen h-screen bg-[url('/bg-noise.gif')] opacity-10 fixed -z-50"></div>
+          <Menu />
+          {children}
+          <Contact />
+        </body>
+      </ReactLenis>
     </html>
-  );
+  )
 }
