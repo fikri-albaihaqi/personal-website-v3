f0092f11780acbd7d5fe47f3f9205b29159d8c0e
fikri-albaihaqi <fikrialbaihaqi@gmail.com>
2025-10-04 22:10:37 +0700

    feat: change header and CTA section text content

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
           data-aos-delay="1000"
           className="absolute hidden md:block bottom-8 min-[1920px]:bottom-32 right-[30%] text-8xl font-semibold mix-blend-exclusion"
         >
-          Front end <br /> Developer
+          Frontend <br /> Developer
         </h1>
 
         <h1 className="md:hidden h-screen flex justify-center items-center text-6xl text-center font-semibold mix-blend-exclusion">
@@ -73,9 +73,13 @@ export default function Home() {
           id="about"
           className="h-screen flex justify-center items-center mx-16 md:mx-0 text-xl md:text-2xl text-center"
         >
-          Hello! I’m Fikri, a 22 years old front end web developer <br />
-          located in Bandung, Indonesia. I like to craft solid and <br />
-          beautiful website with great user experience.
+          <p className="w-full md:w-2/3 xl:w-1/3">
+            Hello! I’m Fikri, a 24 years old passionate Frontend Web Developer
+            who like to build a pixel-perfect and beautiful websites. I'm always
+            stay updated with the latest technologies and trends in the
+            industry. I am dedicated to delivering high-quality solutions that
+            meet and exceed client expectations.
+          </p>
         </section>
 
         <section id="works" className="relative h-max flex flex-col">
@@ -112,11 +116,11 @@ export default function Home() {
         </section>
 
         <section className="relative h-screen flex justify-center items-center mx-16 md:mx-0 text-xl md:text-2xl text-center">
-          <div>
-            I&apos;m currently available <br />
-            and looking for a full-time remote position. <br />
-            If you’d like me to join your team, please reach me out!
-          </div>
+          <p className="w-full md:w-2/3 xl:w-1/3">
+            I'm available for new projects and full-time remote opportunities.
+            If you’re looking for someone to help bring your next website to
+            life, let’s get in touch!
+          </p>
 
           <Link
             href="/Resume_Fikri.pdf"
