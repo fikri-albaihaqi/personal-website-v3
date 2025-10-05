ba38c9988899394280a26982235cb592478fd06b
fikri-albaihaqi <fikrialbaihaqi@gmail.com>
2025-10-05 22:25:10 +0700

    feat: restructure menu list

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import AOS from "aos"
-import "aos/dist/aos.css"
-import { useEffect } from "react";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 export default function Menu() {
   const path = usePathname()
@@ -14,35 +14,74 @@ export default function Menu() {
   }, [])
 
   return (
-    <nav 
+    <nav
       className="fixed w-screen flex justify-between z-50 mt-8 px-8 lg:px-16 text-ivory text-lg mix-blend-difference"
-      data-aos="fade-down" data-aos-duration="1000" data-aos-delay="1500"  
+      data-aos="fade-down"
+      data-aos-duration="1000"
+      data-aos-delay="1500"
     >
       <Link href="/" className="font-semibold text-xl md:text-4xl">
         FA
       </Link>
       <div className="flex text-sm md:text-base">
         <ul className="mr-8">
-          <Link href={path === '/allprojects' ? '/#about' : '#about'} className="hover:text-white transition-all duration-500">
-            <li>About</li>
-          </Link>
-          <Link href={path === '/allprojects' ? '/#works' : '#works'} className="hover:text-white transition-all duration-500">
-            <li>Works</li>
-          </Link>
-          <Link href={path === '/allprojects' ? '/#contact' : '#contact'} className="hover:text-white transition-all duration-500">
-            <li>Contact</li>
-          </Link>
+          <li>
+            <Link
+              href={path === '/allprojects' ? '/#about' : '#about'}
+              className="hover:text-white transition-all duration-500"
+            >
+              About
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href={path === '/allprojects' ? '/#works' : '#works'}
+              className="hover:text-white transition-all duration-500"
+            >
+              Works
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href={path === '/allprojects' ? '/#contact' : '#contact'}
+              className="hover:text-white transition-all duration-500"
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
         <ul>
-          <Link href="/" className="hover:text-white transition-all duration-500">
-            <li>Home</li>
-          </Link>
-          <Link href="/allprojects" className="hover:text-white transition-all duration-500">
-            <li>All Works</li>
-          </Link>
-          <Link href="/Resume_Fikri.pdf" target='_blank' className="hover:text-white transition-all duration-500">
-            <li>My Resume</li>
-          </Link>
+          <li>
+            <Link
+              href="/"
+              className="hover:text-white transition-all duration-500"
+            >
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="/allprojects"
+              className="hover:text-white transition-all duration-500"
+            >
+              All
+            </Link>
+            Works
+          </li>
+
+          <li>
+            <Link
+              href="/Resume_Fikri.pdf"
+              target="_blank"
+              className="hover:text-white transition-all duration-500"
+            >
+              My
+            </Link>
+            Resume
+          </li>
         </ul>
       </div>
     </nav>
