5c07036eae7f4b2203ac3cdcd0c1ff9de07f0e50
fikri-albaihaqi <fikrialbaihaqi@gmail.com>
2026-04-28 11:34:32 +0700

    feat: replace "any" types in selectedProjects component

--- app/components/selectedProjects.tsx
+++ app/components/selectedProjects.tsx
@@ -1,40 +1,37 @@
 'use client'
 
-import CursorFollower from "./cursorFollower"
-import { useRef, useState } from "react"
-import Project from "./project"
-import projects from "../assets/projectsData"
-import { gsap } from "gsap"
-import { useGSAP } from "@gsap/react"
+import CursorFollower from './cursorFollower'
+import { useRef, useState } from 'react'
+import Project from './project'
+import projects from '../assets/projectsData'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const SelectedProjects = () => {
-  const projectsWrapper = useRef<any>()
-  const cursorFollower = useRef<any>()
+  const projectsWrapper = useRef<HTMLDivElement>(null)
+  const cursorFollower = useRef<HTMLDivElement>(null)
 
   const [animation] = useState(gsap.timeline({ paused: true }))
 
   useGSAP(() => {
-    gsap.set(cursorFollower.current, { xPercent: -50, yPercent: -50, scale: 0, opacity: 0 })
+    gsap.set(cursorFollower.current, {
+      xPercent: -50,
+      yPercent: -50,
+      scale: 0,
+      opacity: 0,
+    })
 
-    let x = gsap.quickTo(
-      cursorFollower.current,
-      'x',
-      {
-        duration: 0.6,
-        ease: 'back',
-      }
-    )
+    let x = gsap.quickTo(cursorFollower.current, 'x', {
+      duration: 0.6,
+      ease: 'back',
+    })
 
-    let y = gsap.quickTo(
-      cursorFollower.current,
-      'y',
-      {
-        duration: 0.6,
-        ease: 'back',
-      }
-    )
+    let y = gsap.quickTo(cursorFollower.current, 'y', {
+      duration: 0.6,
+      ease: 'back',
+    })
 
-    window.addEventListener("mousemove", (e) => {
+    window.addEventListener('mousemove', (e) => {
       x(e.clientX)
       y(e.clientY)
     })
@@ -44,22 +41,22 @@ const SelectedProjects = () => {
       {
         scale: 0,
         opacity: 0,
-        duration: 0.3
+        duration: 0.3,
       },
       {
         scale: 1,
         opacity: 100,
-        duration: 0.3
-      },
+        duration: 0.3,
+      }
     )
-    let targets = gsap.utils.toArray(projectsWrapper.current?.children)
+    let targets = gsap.utils.toArray(projectsWrapper.current?.children ?? [])
 
     targets.forEach((target: any) => {
-      target.addEventListener("mouseenter", () => {
+      target.addEventListener('mouseenter', () => {
         animation.play()
       })
 
-      target.addEventListener("mouseleave", () => {
+      target.addEventListener('mouseleave', () => {
         animation.reverse()
       })
     })
@@ -69,7 +66,11 @@ const SelectedProjects = () => {
     <div ref={projectsWrapper} className="flex flex-col items-center">
       <CursorFollower ref={cursorFollower} />
       {projects.slice(0, 5).map((project: any) => (
-        <Project project={project} key={project.id} classes="md:even:self-start md:odd:self-end my-32 md:even:ml-16 md:odd:mr-16" />
+        <Project
+          project={project}
+          key={project.id}
+          classes="md:even:self-start md:odd:self-end my-32 md:even:ml-16 md:odd:mr-16"
+        />
       ))}
     </div>
   )
