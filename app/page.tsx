fa5e003b721b7f66f5edaac2bf44537881d92f7d
fikri-albaihaqi <fikrialbaihaqi@gmail.com>
2026-09-08 12:31:42 +0700

    fix: image position on hero section

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           width={320}
           height={0}
           priority={true}
-          className="absolute self-center md:top-1/3 lg:top-32 min-[1920px]:top-56 min-[2560px]:top-1/3"
+          className="absolute self-center"
           data-aos="zoom-in"
           data-aos-duration="1000"
         />
@@ -35,7 +35,7 @@ export default function Home() {
           data-aos="fade-down"
           data-aos-duration="1000"
           data-aos-delay="1000"
-          className="absolute hidden md:block top-8 min-[1920px]:top-32 min-[2560px]:top-64  left-[30%] text-8xl font-semibold mix-blend-exclusion"
+          className="absolute hidden md:block top-8 min-[1920px]:top-32 min-[2560px]:top-64 left-[30%] text-8xl font-semibold mix-blend-exclusion"
         >
           Fikri <br /> Albaihaqi
         </h1>
